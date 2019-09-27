3f520e9f09b0b2d6fd7be6b8525d304b594c3bba
matigoal <55578343+matigoal@users.noreply.github.com>
2019-09-27 18:26:29 +0200

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -1,7 +1,7 @@
 import requests
-api_address='http://api.openweathermap.org/data/2.5/weather?appid=aaab56eafe22118d2cdf76afcf834026='
-city = input('City Name :')
-url = api_address + city
+api_address='http://api.openweathermap.org/data/2.5/weather?api_key=aaab56eafe22118d2cdf76afcf834026='
+ville = input('Nom de la ville? :')
+url = api_address + ville
 json_data = requests.get(url).json()
 format_add = json_data['base']
 print("Current Temp=",json_data['main']['temp']-273.15)
