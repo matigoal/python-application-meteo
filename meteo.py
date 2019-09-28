9eadf4cd568d75a170255102fa9fdd94156ecaf5
matigoal <55578343+matigoal@users.noreply.github.com>
2019-09-28 19:50:35 +0200

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -1,1 +1,10 @@
 
+import requests
+
+ville = input('Nom de la ville? :')
+api_url ='http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=api_key'
+data = requests.get(api_url)
+result = [data.json()]
+
+
+print(result)
